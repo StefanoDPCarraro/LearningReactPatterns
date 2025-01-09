4efac2435fbe31f73e373241aa701b645d44efc6
Stefano Carraro <stefano.c@edu.pucrs.br>
2025-01-09 20:51:33 -0300

    Added screen spliter new version

--- screen-spliter-enhanced/src/App.jsx
+++ screen-spliter-enhanced/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SplitScreen } from "./components/split-screen";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const LeftSideComponent = () => {
+  return <h2>Left Side</h2>
 }
 
-export default App
+const RightSideComponent = () => {
+  return <h2>Right Side</h2>
+}
+
+function App() {
+  return (
+    <SplitScreen Left={LeftSideComponent} Right={RightSideComponent}/>
+  );
+}
+
+export default App;
