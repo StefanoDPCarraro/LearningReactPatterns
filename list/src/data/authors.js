d7f90cc909f258655709b1083480e8ba3b8a49ad
Stefano Carraro <stefano.c@edu.pucrs.br>
2025-01-09 21:53:11 -0300

    Added template list

---
+++ list/src/data/authors.js
@@ -0,0 +1,14 @@
+export const authors = [
+    {
+        name: "JRR Tolkien",
+        age: 65,
+        country: "South Africa",
+        books: ["Lord of the Rings"]
+    },
+    {
+        name: "Robert C Martin",
+        age: 73,
+        country: "United States",
+        books: ["Clean Code", "Clean Architecture"]
+    }
+]
---
+++ list/src/data/books.js
@@ -0,0 +1,17 @@
+export const books = [
+    {
+        name: "Lord of the Rings",
+        pages: 378,
+        price: 10.99,
+    },
+    {
+        name: "Clean Code",
+        pages: 273,
+        price: 15.99,
+    },
+    {
+        name: "Clean Architecture",
+        pages: 326,
+        price: 19.99,
+    }
+]
